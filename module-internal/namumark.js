ee510409edb050150cd04e0f840f0a06d518ffa4
Basix <jsrtyu1120@gmail.com>
2016-04-24 18:09:22 +0900

    최적화 (아마도)

--- module-internal/namumark.js
+++ module-internal/namumark.js
@@ -16,33 +16,33 @@ module.exports = function(n, ba){
   d('2: '+six)
 
   // 앞 태그
-  six = six.replace(/> (.*)/g, "<blockquote>$1</blockquote>")
+  six = six.replace(/>\s([^\n]*)/g, "<blockquote>$1</blockquote>")
   six = six.replace(/##([^#\n]*)/g, "")
   six = six.replace(/#redirect\s(.*)/g, "<script type=\"text\/javascript\">window.location.href = \"http:\/\/"+wiki.url+"\/w\/$1\"<\/script>")
   d('3: '+six)
 
   // 감싸는 태그
-  six = six.replace(/\'\'\'([^\'\'\']*)\'\'\'/g, "<strong>$1</strong>") // 강조, 굵게
-  six = six.replace(/\'\'([^\'\']*)\'\'/g, "<em>$1</em>") // 이텔릭
-  six = six.replace(/__([^__]*)__/g, "<u>$1</u>") // 밑줄
-  six = six.replace(/--([^--]*)--|~~([^~~]*)~~/g, "<del>$1</del>") // '''취소선'''
-  six = six.replace(/\^\^([^\^\^]*)\^\^/g, "<sup>$1</sup>") // 위첨자
-  six = six.replace(/,,([^\,\,]),,/g, "<sub>$1</sub>") // 아래첨자
+  six = six.replace(/\'\'\'([^\']*)\'\'\'/g, "<strong>$1</strong>") // 강조, 굵게
+  six = six.replace(/\'\'([^\']*)\'\'/g, "<em>$1</em>") // 이텔릭
+  six = six.replace(/__([^_]*)__/g, "<u>$1</u>") // 밑줄
+  six = six.replace(/--([^-]*)--|~~([^~]*)~~/g, "<del>$1</del>") // '''취소선'''
+  six = six.replace(/\^\^([^\^]*)\^\^/g, "<sup>$1</sup>") // 위첨자
+  six = six.replace(/,,([^\,]),,/g, "<sub>$1</sub>") // 아래첨자
   d('4: '+six)
 
   // 제목들
-  six = six.replace(/======\s?([^======]*)\s?======/g, "<h6>$1</h6>")
-  six = six.replace(/=====\s?([^=====]*)\s?=====/g, "<h5>$1</h5>")
-  six = six.replace(/====\s?([^====]*)\s?====/g, "<h4>$1</h4>")
-  six = six.replace(/===\s?([^===]*)\s?===/g, "<h3>$1</h3>")
-  six = six.replace(/==\s?([^==]*)\s?==/g, "<h2>$1</h2>")
+  six = six.replace(/======\s?([^=\s]*)\s?======/g, "<h6>$1</h6>")
+  six = six.replace(/=====\s?([^=\s]*)\s?=====/g, "<h5>$1</h5>")
+  six = six.replace(/====\s?([^=\s]*)\s?====/g, "<h4>$1</h4>")
+  six = six.replace(/===\s?([^=\s]*)\s?===/g, "<h3>$1</h3>")
+  six = six.replace(/==\s?([^=\s]*)\s?==/g, "<h2>$1</h2>")
   d('5: '+six)
 
   // 고급 태그
-  six = six.replace(/\[\[([^[[\]\]]*)\|([^[[\]\]]*)]]/g, "<a href=\"/w/$1\">$2</a>") // 커스텀 이름의 링크
-  six = six.replace(/\[\[([^[[\]\]]*)]]/g, "<a href=\"/w/$1\">$1</a>") // 링크
+  six = six.replace(/\[\[([^\[\]]*)\|([^\[\]]*)]]/g, "<a href=\"/w/$1\">$2</a>") // 커스텀 이름의 링크
+  six = six.replace(/\[\[([^[\[\]]*)]]/g, "<a href=\"/w/$1\">$1</a>") // 링크
   six = six.replace(/([^\n]*\.(jpeg|jpg|gif|png))/g, "<img src=\"$1\">") // 이미지
-  six = six.replace(/\{{{([^]*)}}}/g, "<code>$1</code>") // 코드로 바꾸기만 지원
+  six = six.replace(/\{{{(.*)}}}/g, "<code>$1</code>") // 코드로 바꾸기만 지원
   six = six.replace(/-{4,11}/g, "<hr>") // 수평선
   d('6: '+six)
 
